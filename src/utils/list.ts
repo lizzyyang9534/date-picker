a5f85f368466a072838b64be5c5094f16ffac6a7
LizzyYang <lizzyyang9534@gmail.com>
2023-01-13 22:46:40 +0800

    doc: add description of range function

--- src/utils/list.ts
+++ src/utils/list.ts
@@ -1,3 +1,12 @@
+/**
+ * @param from start number
+ * @param to end number
+ * @returns An array of numbers from `from` to `to`.
+ * @example
+ *     range(1, 3) => [1, 2, 3]
+ *     range(3, 1) => [3, 2, 1]
+ *     range(1, 1) => [1]
+ */
 export const range = (from: number, to: number): number[] => {
   const result: number[] = [];
   if (from <= to) {
