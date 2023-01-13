c39d2ea77152eda230cb7b8a8ecd9c02912fe174
LizzyYang <lizzyyang9534@gmail.com>
2023-01-13 21:57:48 +0800

    feat: adjust brand color scheme and add comment

--- src/theme.ts
+++ src/theme.ts
@@ -3,12 +3,12 @@ import { extendTheme } from '@chakra-ui/react';
 const theme = extendTheme({
   colors: {
     brand: {
-      500: '#db3d44',
+      500: '#d73a42',
       600: '#db3d44',
-      700: '#db3d44',
+      700: '#b92f36',
     },
     brandGray: {
-      500: '#eee',
+      500: '#eee', // This color might be too light to see clearly, so I use #ccc as text color instead
       600: '#e9e9e9',
       700: '#ccc',
     },
